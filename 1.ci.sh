2fc248a3ce2242e43cde4f745c555fc652b6aad3
gnuhub <gnuhub@qq.com>
2024-05-11 14:08:53 +0800

    [just fix it]

---
+++ 1.ci.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export PROJECT_NAME="${CMD_PATH##*/}"
+echo $PROJECT_NAME
+cd $CMD_PATH
+
+echo "hello world"
+cd ~
+git clone -b dev_240511_03 --depth 1 https://github.com/es366/10004-kibana.git kibana
+cd kibana
+yarn build --skip-os-packages
+
+ls -al
